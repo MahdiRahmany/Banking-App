626a301bdd5ac6a57ca4be3e0d7aa784c98981f0
Mahdi Rahmani <mahdirahmani.cm@gmail.com>
2024-09-22 01:54:45 +0330

    home page ui

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,6 +1,27 @@
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+
 const Home = () => {
+  const loggedIn = { firstName: "User" };
+
   return (
-    <div>Home</div>
-  )
-}
-export default Home
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transactions efficiently"
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+export default Home;
